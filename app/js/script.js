81002d507792cfa2260348fb684f1df4edf3ef48
Jeptzy <77440578+Jeptzy@users.noreply.github.com>
2021-03-20 22:34:31 +0800

    adding close burger menu and nav item

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,23 @@
 const burger = document.querySelector('.burger');
 const navMenu = document.querySelector('.nav_menu')
+const burgerClose = document.querySelector('.burger_close')
+const navItem = document.querySelectorAll('.nav_item')
 
+//show nav
 burger.addEventListener('click', function(){
-    navMenu.classList.toggle('show_nav')
+    navMenu.classList.add('show_nav')
 })
+
+//remove nav
+burgerClose.addEventListener('click', function(){
+    navMenu.classList.remove('show_nav')
+})
+
+function linkAction(){
+    navItem.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+    navMenu.classList.remove('show_nav');
+    
+}
+
+navItem.forEach(n=> n.addEventListener('click', linkAction))
