01d85602842145b71ba045efabd0601ac41be0e1
Jeptzy <77440578+Jeptzy@users.noreply.github.com>
2021-03-21 22:14:32 +0800

    adding features

--- app/js/script.js
+++ app/js/script.js
@@ -6,11 +6,13 @@ const navItem = document.querySelectorAll('.nav_item')
 //show nav
 burger.addEventListener('click', function(){
     navMenu.classList.add('show_nav')
+    
 })
 
 //remove nav
 burgerClose.addEventListener('click', function(){
     navMenu.classList.remove('show_nav')
+   
 })
 
 function linkAction(){
@@ -18,6 +20,41 @@ function linkAction(){
     this.classList.add('active');
     navMenu.classList.remove('show_nav');
     
+    
 }
 
-navItem.forEach(n=> n.addEventListener('click', linkAction))
+navItem.forEach(n=> n.addEventListener('click', linkAction));
+
+
+//tabs
+const tabLink = document.querySelectorAll('.tab');
+
+function tabAction(){
+  tabLink.forEach(n => n.classList.remove('tab_active'));
+  this.classList.add('tab_active');
+}
+
+tabLink.forEach(n=> n.addEventListener('click', tabAction))
+
+
+function openPage(pageName) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent;
+
+
+    tabcontent = document.getElementsByClassName("tab_desc");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "flex";
+  
+    // Add the specific color to the button used to open the tab content
+  
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
+ 
